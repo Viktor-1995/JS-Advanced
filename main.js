4221c316a0f8ee52135ef40ead4fa9e6ff05295d
Viktor-1995 <madebyvityok@ukr.net>
2020-11-05 10:23:50 +0200

    make all cnahges

--- main.js
+++ main.js
@@ -1,19 +1,15 @@
 /*
-
   Задание 1.
-
   Написать функцию которая будет задавать СЛУЧАЙНЫЙ цвет для фона.
   Каждая перезагрузка страницы будет с новым цветом.
   Для написания используйте функцию на получение случайного целого числа,
   между минимальным и максимальным значением (Приложена снизу задания)
-
   + Бонус, повесить обработчик на кнопку через метод onClick
   + Бонус, использовать 16-ричную систему исчесления и цвет HEX -> #FFCC00
   + Бонус выводить полученый цвет по центру страницы.
   
   Необходимо создать блок через createElement задать ему стили через element.style
   и вывести через appendChild или insertBefore
-
   Необходимые материалы:
     Math.Random (Доки): https://developer.mozilla.org/uk/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     function getRandomIntInclusive(min, max) {
@@ -26,17 +22,14 @@
     Вариант 1.
       Исользовать element.style.background = 'rgb(r,g,b)';
       где r,g,b случайное число от 0 до 255;
-
     Вариант 2.
       Исользовать element.style.background = '#RRGGBB';
       где, RR, GG, BB, значние цвета в 16-ричной системе исчесления
       Формирование цвета в вебе: https://ru.wikipedia.org/wiki/%D0%A6%D0%B2%D0%B5%D1%82%D0%B0_HTML
       Перевод в 16-ричную систему исчесления делается при помощи
       метода Number.toString( 16 ) https://www.w3schools.com/jsref/jsref_tostring_number.asp,
-
       var myNumber = '251'
       myNumber.toString(16) // fb
-
 */
 var max = 255;
 var c;
@@ -46,7 +39,15 @@ var c;
     var g = getRandomInt(max);
     var b = getRandomInt(max);
 
-    document.body.style.background = "rgb( " + r + "," + g + "," + b + ")" ;
+    var div = document.createElement('div');
+    var span = document.createElement('span');
+    span.style = 'position:absolute;top:50%;width:100%;text-align:center;';
+
+
+    document.body.style.background = "#" + Number(r).toString( 16 ) + Number(g).toString( 16 ) + Number(b).toString( 16 ) ;
+    var c = Number(r).toString( 16 ) + Number(g).toString( 16 ) + Number(b).toString( 16 );
+    span.innerText = 'now color of background #' + c ;
+  
 
   function getRandomInt(max) {
 
@@ -54,9 +55,6 @@ var c;
 
     }
 
-var div = document.createElement('div');
-var span = document.createElement('span');
-span.style = 'position:absolute;top:50%;width:100%;text-align:center;';
 
   function changeColor(  ){
 
@@ -73,7 +71,4 @@ span.style = 'position:absolute;top:50%;width:100%;text-align:center;';
   
  div.appendChild(span);
 document.body.appendChild(div);
-
-
-
 
